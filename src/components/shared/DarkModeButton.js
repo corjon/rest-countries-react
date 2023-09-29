92735723cdb3451d1f448e689a710da6fe44d864
corjon <cordesjonathan@gmail.com>
2023-09-28 19:05:29 -0700

    Add static shared components

---
+++ src/components/shared/DarkModeButton.js
@@ -0,0 +1,7 @@
+export default function DarkModeButton() {
+  return (
+    <>
+      <button>Dark Mode</button>
+    </>
+  );
+}
---
+++ src/components/shared/Header.js
@@ -0,0 +1,10 @@
+import DarkModeButton from './DarkModeButton.js';
+
+export default function Header() {
+  return (
+    <header>
+      <h1>Where in the world?</h1>
+      <DarkModeButton />
+    </header>
+  );
+}
